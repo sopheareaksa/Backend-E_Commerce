45b009432844c7d9ea35b1aed4b126096c3aef82
Sophea Reaksa <sopheareaksapheak@gmail.com>
2026-07-04 15:17:11 +0700

    Backend

---
+++ config/cors.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://frontend-e-commerce-m2na.vercel.app', // Your live Vercel Link
+        'http://localhost:5173',                       // Keep this for local testing
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
