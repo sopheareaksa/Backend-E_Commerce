85e81c94e8e3ebf6cad7540bc2a883b830749969
Sophea Reaksa <Sopheareaksapheak@gmail.com>
2026-08-23 23:17:46 +0700

    Backend E-Commerce

--- config/cors.php
+++ config/cors.php
@@ -1,4 +1,18 @@
-'paths' => ['api/*', 'sanctum/csrf-cookie'],
-'allowed_methods' => ['*'],
-'allowed_origins' => ['https://frontend-e-commerce-m2na.vercel.app/'],
-'supports_credentials' => true,
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://frontend-e-commerce-m2na.vercel.app',
+        'http://localhost:5173',
+    ],
+
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
